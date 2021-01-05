f5b239d22233e6073b0c24a5947b666e09d20662
Daniel Zárate <49570493+Danifado@users.noreply.github.com>
2021-01-05 16:42:14 -0500

    Finalización feature "Chatea con nosotros"
    Se termina el "Chatea con nosotros" de whatsapp, queda pendiente el redireccionamiento por medio de la API de WhatsApp

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
-$(function () {
+whatsIcon = document.getElementsByClassName("whatsIcon")[0];
+whatsTr = document.getElementsByClassName("whatsTr")[0];
+whatsText = document.getElementsByClassName("whatsText")[0];
 
-    var $gallery = $('.gallery').flickity({
-        cellSelector: '.gallery-cell'
-    })
+function whatsOn(){
+    whatsIcon.src = './Assets/SVG/whatsGreenIcon.svg';
+    whatsTr.style.width = '30vh';
+    whatsTr.style.backgroundColor ='#25D366';
+    console.log("sisisi");
+    setTimeout(()=>{
+        setTimeout(() => {
+            whatsText.style.visibility = 'visible'
+        }, 200);
+        // Wrap every letter in a span
+        var textWrapper = document.querySelector('.whatsText .letters');
+        textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-});
+        anime.timeline({ loop: false })
+            .add({
+                targets: '.whatsText .letter',
+                scale: [0, 1],
+                duration: 1500,
+                elasticity: 600,
+                delay: (el, i) => 45 * (i + 1)
+            })
+    }, 200);
+}
+
+function whatsOff(){
+    whatsIcon.src = './Assets/SVG/whatsGrayIcon.svg';
+    console.log("ñoñoñoño");
+    whatsTr.style.width = '5vh';
+    whatsTr.style.backgroundColor = 'gray';
+    // setTimeout(() => {
+        whatsText.style.visibility = 'hidden'
+    // }, 200);
+}
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.whatsText .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
