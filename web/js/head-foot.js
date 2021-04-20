0a60dfdcf27e2de0500b6b4b03fe027c3b5312e8
Daniel Zárate <danifado@gmail.com>
2021-04-20 11:47:30 -0500

    Arreglo overlay f-center y f-right

--- web/js/head-foot.js
+++ web/js/head-foot.js
@@ -1,15 +1,29 @@
 fetch("./head-foot/header.html")
-  .then(response => {
-    return response.text()
-  })
-  .then(data => {
-    document.querySelector("header").innerHTML = data;
-  });
+.then(response => {
+  return response.text()
+})
+.then(data => {
+  document.querySelector("header").innerHTML = data;
+});
 
-  fetch("./head-foot/footer.html")
-  .then(response => {
-    return response.text()
-  })
-  .then(data => {
-    document.querySelector("footer").innerHTML = data;
-  });
+fetch("./head-foot/footer.html")
+.then(response => {
+  return response.text()
+})
+.then(data => {
+  document.querySelector("footer").innerHTML = data;
+});
+
+setTimeout(5000);
+
+function changeBotRight(mediaSmall){
+  // console.log(botRightImg.src);
+  // console.log("holis")
+
+}
+
+var mediaSmall =  window.matchMedia("(max-width: 768px)");
+
+
+changeBotRight(mediaSmall);
+setInterval(changeBotRight(), 1000);
