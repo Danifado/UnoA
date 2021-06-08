407550baed2a7cfd6a0d24299860eb82a85e160e
Daniel Zárate <danifado@gmail.com>
2021-06-08 14:12:51 -0500

    Versión de escritorio catálogo interactivo (Flipbook with Turn JS)

--- web/js/flip.js
+++ web/js/flip.js
@@ -1,7 +1,31 @@
+const pcMediaQuery = window.matchMedia("(max-width: 768px)");
+
+function flipbookImportHandler(e) {
+if (e.matches) {
+    $('flipbook')[0].style.display = "None";
+    $('flipbook-mobile')[0].style.display = "Block"
+} else {
+    $('flipbook')[0].style.display = "Block";
+    $('flipbook-mobile')[0].style.display = "None"
+}
+}
+
 fetch("/flipbook/flipbook.html")
 .then(response => {
     return response.text()
 })
 .then(data => {
-    document.querySelector("#pc-flipbook").innerHTML = data;
+    document.querySelector("flipbook").innerHTML = data;
 });
+
+fetch("/flipbook/flipbook-mobile.html")
+.then(response => {
+    return response.text()
+})
+.then(data => {
+    document.querySelector("flipbook-mobile").innerHTML = data;
+});
+
+pcMediaQuery.addListener(flipbookImportHandler);
+
+flipbookImportHandler(pcMediaQuery);
